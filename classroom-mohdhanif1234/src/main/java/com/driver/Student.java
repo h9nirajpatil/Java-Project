f9b44813c20b543b0ce7f3bdc0a5d1756fb8f3a5
h9nirajpatil <156055885+h9nirajpatil@users.noreply.github.com>
2024-06-14 09:51:44 +0530

    Add files via upload

---
+++ classroom-mohdhanif1234/src/main/java/com/driver/Student.java
@@ -0,0 +1,42 @@
+package com.driver;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double averageScore;
+
+    public Student(){
+
+    }
+
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+}
